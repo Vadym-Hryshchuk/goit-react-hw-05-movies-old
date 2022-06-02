edd1a033ba39ef992f060e02270e31a1a1cc55b5
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2022-06-02 21:50:46 +0300

    add fix

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App/App';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
